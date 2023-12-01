8ca1eda7429b281db709fc1e4f969cfd0255e1f9
pselloni <kasenkolev318@gmail.com>
2023-12-01 13:36:42 +0300

    feat: added base to memorize answers

--- main.py
+++ main.py
@@ -3,23 +3,10 @@ import pyttsx3
 import speech_recognition as sr
 import random
 import requests
+import json
+from difflib import get_close_matches
 import subprocess
-
-#блок очко
-'''from openai import OpenAI
-
-client = OpenAI(api_key="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")
-
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
-
-print(completion.choices[0].message)'''
-
+from transformers import AutoTokenizer, AutoModelWithLMHead
 
 open_weather_token = 'a47d6ab82e58378d27beefdd5318771c'
 city = "Moscow"
@@ -32,12 +19,34 @@ engine.setProperty('rate', 40)
 engine.setProperty('volume', 0.9)
 engine.setProperty('voice', 'com.apple.voice.compact.ru-RU.Milena')
 
+r = sr.Recognizer()
+
 #moods
 mood = ['Отлично', 'Всё в порядке', 'Отстань', 'пока не родила азазазазаз']
 
-def record_volume():
-    r = sr.Recognizer()
+def load_knowledge_basis(file_path: str):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
 
+def save_knowledge_base(filepath: str, data: dict):
+    with open(filepath, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def find_friends(user_input: str, questions: list[str]):
+    matches = get_close_matches(user_input, questions, n=1, cutoff=0.6)
+
+    if matches:
+        return matches[0]
+    else:
+        return None
+
+def get_answer(question: str, knowledge_base: dict):
+    for val in knowledge_base['questions']:
+        if val['question'] == question:
+            return val['answer']
+
+def recognize_text(flag=False):
     with sr.Microphone(device_index=0) as source:
         print('Настраиваюсь.')
         r.adjust_for_ambient_noise(source, duration=0.5)
@@ -47,42 +56,44 @@ def record_volume():
     print('Услышала.')
 
     try:
-        ans = ''
-
         query = r.recognize_google(audio, language='ru-RU')
-        text = query.lower()
-
-        #checking basic scenarios
-        if 'привет' in text or 'Добрый' in text:
-            ans += 'Здравствуйте'
-
-        if 'как' in text and 'дела' in text:
-            ans += (random.choice(mood) + 'а у вас')
-
-        if 'я' in text and 'люблю' in text and 'тебя' in text:
-            ans += ('Если ваш день вдруг станет пасмурным, подойдите к зеркалу и увидите солнышко')
-
-        if 'погода' in text or 'температура' in text:
-            result = requests.get(
-                f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-            )
-            data = result.json()
-            ans += ("Температура в Москве " + str(
-                data['main']['temp']) + "Ощущается как " + str(data['main']['feels_like']))
-        if 'telegram' in text:
-            subprocess.Popen(["open", '/Applications/Telegram.app'])
-
-        #ans += model_reply(text)
-
-
-        #model replies
-        engine.say(ans)
+        return query.lower()
     except:
         print('Error')
 
+def brain():
+    base = load_knowledge_basis('knowledge_basis.json')
+    ans = ''
+
+    text = recognize_text()
+
+    best_match = find_friends(text, [val['question'] for val in base['questions']])
+
+    if 'погода' in text or 'температура' in text:
+        result = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
+        )
+        data = result.json()
+        engine.say("Температура в Москве " + str(
+            data['main']['temp']) + "Ощущается как " + str(data['main']['feels_like']))
+    elif 'telegram' in text:
+        engine.say('Запускаю телеграм')
+        subprocess.Popen(["open", '/Applications/Telegram.app'])
+    else:
+        if best_match:
+            engine.say(get_answer(best_match, base))
+        else:
+            engine.say('Я не знаю что ответить, какой правильный ответ?')
+            engine.runAndWait()
+            curr_ans = recognize_text()
+
+            base['questions'].append({'question': text, 'answer': curr_ans})
+            save_knowledge_base('knowledge_basis.json', base)
+            engine.say('Спасибо, моя база знаний пополнилась')
+
 def main():
     while True:
-        #record_volume()
+        brain()
         engine.runAndWait()
 
 if __name__ == '__main__':
